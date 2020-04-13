6a02b0f872039cd1385f54ec09eb939974e0db79
Fraser Cobb <frasercobb@gmail.com>
2020-04-13 14:12:16 +1000

    refactor: split prompter tests by colour

--- prompter_test.go
+++ prompter_test.go
@@ -7,14 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_CreateSelectOptions(t *testing.T) {
+func Test_CreateSelectOptionsColoursPatch(t *testing.T) {
 	modules := []Module{
-		{
-			Name:         "foo/bar",
-			FromVersion:  semver.MustParse("0.0.1"),
-			ToVersion:    semver.MustParse("0.0.2"),
-			MinorUpgrade: true,
-		},
 		{
 			Name:         "frasercobb/gomo",
 			FromVersion:  semver.MustParse("1.2.3"),
@@ -25,8 +19,23 @@ func Test_CreateSelectOptions(t *testing.T) {
 	result := createSelectOptions(modules)
 
 	assert.Equal(t, []string{
-		"\x1b[34mfoo/bar 0.0.1 -> 0.0.2\x1b[0m",
 		"\x1b[32mfrasercobb/gomo 1.2.3 -> 1.3.4\x1b[0m",
+	}, result)
+}
+
+func Test_CreateSelectOptionsColoursMinor(t *testing.T) {
+	modules := []Module{
+		{
+			Name:         "foo/bar",
+			FromVersion:  semver.MustParse("0.0.1"),
+			ToVersion:    semver.MustParse("0.0.2"),
+			MinorUpgrade: true,
+		},
+	}
+	result := createSelectOptions(modules)
+
+	assert.Equal(t, []string{
+		"\x1b[34mfoo/bar 0.0.1 -> 0.0.2\x1b[0m",
 	}, result)
 }
 
